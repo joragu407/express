752ad05749f2d27764494790e246776e3e3b87af
joragu407 407 <jorge407@gmail.com>
2024-10-29 22:30:00 +0000

    send view again

--- myApp.js
+++ myApp.js
@@ -7,7 +7,7 @@ let app = express();
   });
 */
 app.get("/", function(req, res) {
-    res.sendFile(__dirname + "/public/views/index.html");
+    res.sendFile(__dirname + "/views/index.html");
   });
 
 
