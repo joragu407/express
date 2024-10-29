6d410ebd3ac8c6cf023a43952886f8488b66e894
joragu407 407 <jorge407@gmail.com>
2024-10-29 22:27:37 +0000

    send index

--- myApp.js
+++ myApp.js
@@ -2,10 +2,10 @@ let express = require('express');
 let app = express();
 
 
-app.get("/", function(req, res) {
+/*app.get("/", function(req, res) {
     res.send("Hello Express");
   });
-
+*/
 app.get("/", function(req, res) {
     res.sendFile(__dirname + "/public/index.html");
   });
